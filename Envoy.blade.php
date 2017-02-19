346c2ba6573f1748b8c592f6cab839076cb85954
Olav Schettler <olav@schettler.net>
2017-02-19 22:03:59 +0100

    npm run production

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -43,7 +43,7 @@
     php artisan migrate
 
     npm install
-    node node_modules/gulp/bin/gulp.js --production
+    npm run production
 
     #chmod 777 storage/logs/laravel.log
     #sudo setfacl -R -m u:www-data:rwX -m u:olav:rwX {{ $root_dir }}/shared/storage
