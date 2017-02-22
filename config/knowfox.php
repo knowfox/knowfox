7dd19f47c3cc3540128d5a8e4dae55d47827e588
Olav Schettler <olav@schettler.net>
2017-02-22 17:59:42 +0100

    More relationships

--- config/knowfox.php
+++ config/knowfox.php
@@ -3,13 +3,14 @@
 return [
     'relationships' => [
         'commission' => [ 'beauftragt', 'beauftragt von' ],
-        'written' => [ 'hat geschrieben', 'wurde geschrieben von' ],
         'invented' => [ 'hat erfunden', 'wurde erfunden von' ],
+        'platform' => [ 'stellt Plattform bereit für', 'nutzt Plattform von' ],
         'similar' => [ 'ähnlich mit', 'ähnlich mit' ],
         'supports' => [ 'unterstützt', 'unterstützt von' ],
         'teacher' => [ 'Lehrer von', 'Schüler von' ],
         'translates' => [ 'übersetzt', 'übersetzt' ],
         'uses' => ['nutzt', 'wird genutzt von'],
+        'written' => [ 'hat geschrieben', 'wurde geschrieben von' ],
     ],
 
     'languages' => [
