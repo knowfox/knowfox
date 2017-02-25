a5aca3b1fef7a3c00a8273b568dc3fe4ebb93f83
Olav Schettler <olav@schettler.net>
2017-02-25 16:27:47 +0100

    Bookmarklet HTML

--- resources/views/bookmark/create.blade.php
+++ resources/views/bookmark/create.blade.php
@@ -1,11 +1,19 @@
-<form id="knowfox-bookmark" action="{{route('bookmark.store')}}" method="POST">
-    {{csrf_field()}}
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Bookmarklet | Knowfox</title>
+</head>
+<body>
+    <form id="knowfox-bookmark" action="{{route('bookmark.store')}}" method="POST">
+        {{csrf_field()}}
 
-    <input name="title" value="{{$concept->title}}">
-    <input name="source_url" value="{{$concept->source_url}}">
+        <input name="title" value="{{$concept->title}}">
+        <input name="source_url" value="{{$concept->source_url}}">
 
-    <button type="submit">Save</button>
-</form>
-<script>
-    //var document.getElementById('knowfox-bookmark').submit();
-</script>
+        <button type="submit">Save</button>
+    </form>
+    <script>
+        document.getElementById('knowfox-bookmark').submit();
+    </script>
+</body>
+</html>
--- resources/views/bookmark/show.blade.php
+++ resources/views/bookmark/show.blade.php
@@ -1,1 +1,9 @@
-<a target="_top" href="https://knowfox.com/concept/{{$concept->id}}">Bookmarked "{{$concept->title}}"</a>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Bookmarklet | Knowfox</title>
+</head>
+<body>
+    <a style="font-family:helvetica sans-serif" target="_top" href="https://knowfox.com/concept/{{$concept->id}}">Bookmarked "{{$concept->title}}"</a>
+</body>
+</html>
