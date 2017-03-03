f56f45169f0b49b6418b22abeb951ab1a29d4a57
Olav Schettler <olav@schettler.net>
2017-03-03 12:10:28 +0100

    Friendlier login mail

--- resources/views/auth/emails/email-login.blade.php
+++ resources/views/auth/emails/email-login.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.email')
 
 @section('content')
+    Hello {{$user->name}},
+
+    click on the button to log into Knowfox:
+
     <a class="btn btn-default" href="{{ $url }}">Log into Knowfox</a>
+
+    If this button does not work, please try again or contact us at hello@post.knowfox.com.
+
+    Cheers,
+    -- The Knowfox Messenger
 @endsection
