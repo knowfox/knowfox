eb68d765cb31e26e7b217a88e3cd00c6c21c9795
Olav Schettler <olav@schettler.net>
2017-03-06 21:21:06 +0100

    GPLv3; Evernote filenames slugified

--- app/Console/Commands/ImportEvernote.php
+++ app/Console/Commands/ImportEvernote.php
@@ -1,5 +1,21 @@
 <?php
-
+/**
+ * Knowfox - Personal Knowledge Management
+ * Copyright (C) 2017  Olav Schettler
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 namespace Knowfox\Console\Commands;
 
 use EDAM\Error\EDAMErrorCode;
@@ -9,6 +25,7 @@ use EDAM\NoteStore\NotesMetadataResultSpec;
 use Evernote\Client;
 use Illuminate\Console\Command;
 use Illuminate\Http\File;
+use Illuminate\Support\Str;
 use Knowfox\Models\Concept;
 use Knowfox\Services\PictureService;
 use DOMDocument;
@@ -136,7 +153,7 @@ class ImportEvernote extends Command
         if ($note->resources) {
             foreach ($note->resources as $resource) {
                 $hash = bin2hex($resource->data->bodyHash);
-                $filename = $resource->attributes->fileName;
+                $filename = Str::slug($resource->attributes->fileName);
                 if (!$filename) {
                     $guesser = new MimeTypeExtensionGuesser();
                     $filename = $hash . '.' . $guesser->guess($resource->mime);
