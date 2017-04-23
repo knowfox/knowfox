09e89594896b84a48a1837a74f6c59a93332f37f
Olav Schettler <olav@schettler.net>
2017-04-23 22:19:08 +0200

    Same template for blog

--- resources/views/website/blog_knowfox_com/fragment.blade.php
+++ resources/views/website/blog_knowfox_com/fragment.blade.php
@@ -1,5 +1,10 @@
 <div class="blog-post">
-    <h2 class="blog-post-title"><a href="/{{$concept->slug}}">{{$concept->title}}</a></h2>
+    @if ($concept->image_src)
+        <a href="/{{$concept->slug}}/">
+            <img class="blog-post-thumbnail thumbnail" src="{{$concept->image_src}}">
+        </a>
+    @endif
+    <h2 class="blog-post-title"><a href="/{{$concept->slug}}/">{{$concept->title}}</a></h2>
     <p class="blog-post-meta">{{ strftime('%Y-%m-%d', strtotime($concept->created_at)) }} by {{$concept->owner->name}}</p>
 
     <p>{{$concept->summary}}</p>
