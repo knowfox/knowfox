f3b35a2d1e1226e1e2e20c4a08f9894fc5c5bf6e
Olav Schettler <olav@schettler.net>
2017-05-05 00:54:03 +0200

    Tags and flags in books

--- resources/views/concept/show-book-list.blade.php
+++ resources/views/concept/show-book-list.blade.php
@@ -40,6 +40,7 @@
             $books = $books
                 ->orderBy('title', 'asc')
                 ->paginate();
+
             if (Input::has('letter')) {
                 $books->appends('letter', $letter);
             }
@@ -73,6 +74,14 @@
                     <td><a href="{{route('concept.show', ['concept' => $book])}}">
                         {{$book->title}}
                         </a>
+                        @if ($book->is_flagged)
+                            <i class="glyphicon glyphicon-heart"></i>
+                        @endif
+                        <?php $sep = '<br>'; ?>
+                        @foreach ($book->tags as $tag)
+                            {!! $sep !!}<a href="{{route('concept.index', ['tag' => $tag->slug])}}" class="label label-default">{{$tag->name}}</a>
+                            <?php $sep = ''; ?>
+                        @endforeach
                     </td>
                     <td>{{$book->config->year or ''}}</td>
                 </tr>
