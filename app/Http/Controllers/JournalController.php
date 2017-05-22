c94e6994d97ca6aa2380baa136ccb50159ed5827
Olav Schettler <olav@schettler.net>
2017-05-22 07:53:12 +0200

    Navigation with monday and friday

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -11,6 +11,7 @@ class JournalController extends Controller
 {
     public function date($date_string = null)
     {
+        /** @var \Carbon\Carbon $date */
         if ($date_string) {
             $date = Carbon::createFromFormat('Y-m-d', $date_string);
             if (!$date) {
@@ -42,13 +43,27 @@ class JournalController extends Controller
             ->where('title', 'like', $date->format('Y-m-d') . '%')
             ->first();
         if (!$concept) {
+            $nav = '';
+
+            if ($date->dayOfWeek == Carbon::MONDAY) {
+                $friday = (clone $date)->subDay(3)->format('Y-m-d');
+                $nav .= "[friday ({$friday})](/{$friday}) | ";
+            }
+
             $prev = (clone $date)->subDay(1)->format('Y-m-d');
             $next = (clone $date)->addDay(1)->format('Y-m-d');
+            $nav .= "[yesterday ({$prev})](/{$prev}) | [tomorrow ({$next})](/{$next})";
+
+            if ($date->dayOfWeek == Carbon::FRIDAY) {
+                $monday = (clone $date)->addDay(3)->format('Y-m-d');
+                $nav .= "| [monday ({$monday})](/{$monday})";
+            }
+
             $concept = Concept::create([
                 'parent_id' => $month->id,
                 'title' => $date->format('Y-m-d l:') . ' Journal',
                 'owner_id' => Auth::id(),
-                'body' => "[yesterday ({$prev})](/journal/{$prev}) | [tomorrow ({$next})](/journal/{$next})\n\n",
+                'body' => $nav . "\n\n",
             ]);
             $concept->tag('Journal');
         }
