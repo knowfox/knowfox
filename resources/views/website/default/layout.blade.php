dedd27493a6ec1da32ea2d7d8a93b3e20c9d9ce5
Olav Schettler <olav@schettler.net>
2017-07-22 23:42:20 +0200

    Hypothesis as commenting system

--- resources/views/website/default/layout.blade.php
+++ resources/views/website/default/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{$lang or 'en'}}"{!! $html_attr or '' !!}>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,13 +10,13 @@
     <meta name="generator" content="https://knowfox.com">
     <link rel="icon" href="/favicon.ico">
 
-    <title>{{$config->title}}</title>
+    <title>{{$concept->title != $config->title ? "{$concept->title} | " : ''}}{{$config->title}}</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
     <link href="/css/blog.css" rel="stylesheet">
+
+    @stack('header')
 </head>
 
 <body>
--- resources/views/website/schettler_net/layout.blade.php
+++ resources/views/website/schettler_net/layout.blade.php
@@ -1,1 +1,13 @@
 @extends('website.default.layout')
+
+@push('header')
+    <meta property="og:title" content="{{$concept->title}}" />
+    <meta property="og:site_name" content="{{$config->title}}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:locale" content="de_DE" />
+    <link rel="canonical" href="https://schettler.net{{$concept->url}}" />
+@endpush
+
+@push('scripts')
+    <script src="https://hypothes.is/embed.js" async></script>
+@endpush
