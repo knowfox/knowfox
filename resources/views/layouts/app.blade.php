7c88652309919739d2b3c85a51bddc72b747b7f4
Olav Schettler <olav@schettler.net>
2017-07-29 11:53:27 +0200

    Knowfox in search console

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="_KtWrrDb_zGhZ_GEtxg5LOYfbrX50SV1b4P3ZDVNQVg" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
--- resources/views/website/blog_knowfox_com/layout.blade.php
+++ resources/views/website/blog_knowfox_com/layout.blade.php
@@ -1,1 +1,10 @@
 @extends('website.default.layout')
+
+@push('header')
+    <meta name="google-site-verification" content="_KtWrrDb_zGhZ_GEtxg5LOYfbrX50SV1b4P3ZDVNQVg" />
+    <meta property="og:title" content="{{$concept->title}}" />
+    <meta property="og:site_name" content="{{$config->title}}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:locale" content="en_US" />
+    <link rel="canonical" href="https://blog.knowfox.com{{$concept->url}}" />
+@endpush
