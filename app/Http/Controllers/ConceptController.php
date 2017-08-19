054c83878e2f4d2ca8f91064a45079c9e31c2c50
Olav Schettler <olav@schettler.net>
2017-08-19 11:57:23 +0200

    Default upload: square

--- app/Http/Controllers/ConceptController.php
+++ app/Http/Controllers/ConceptController.php
@@ -393,7 +393,7 @@ class ConceptController extends Controller
             $parts = pathinfo($path);
 
             if (strpos($concept->body, $parts['basename']) === false) {
-                $concept->body .= "\n\n<a data-featherlight=\"{$parts['basename']}\">![{$parts['filename']}]({$parts['basename']}?style=thumbnail)</a>\n";
+                $concept->body .= "\n\n<a data-featherlight=\"{$parts['basename']}\">![{$parts['filename']}]({$parts['basename']}?style=square)</a>\n";
                 $concept->save();
             }
         }
