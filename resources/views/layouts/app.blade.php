20087d0676e537f7e623fcefce58c0568b1333a4
Olav Schettler <olav@schettler.net>
2017-12-13 07:27:07 +0100

    Link to blog

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -134,6 +134,7 @@
             <p class="text-muted">
                 &copy; {{ date('Y') }} Dr. Olav Schettler |
                 <a href="javascript:(function(){d=document.createElement('iframe');d.style='position:fixed;z-index:9999;top:10px;right:10px;width:200px;height:200px;background:#FFF;';d.src='https://knowfox.com/bookmark?url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title);document.body.appendChild(d);})()"><i class="glyphicon glyphicon-bookmark"></i><span class="desktop-only"> Bookmarklet</span></a>
+                | <a href="https://blog.knowfox.com">Blog</a>
                 | <a href="https://knowfox.com/presentation/47d6c8de/013c/11e7/8a8c/56847afe9799/index.html">Features</a>
                 | <a href="https://github.com/oschettler/knowfox/wiki">Getting started</a>
                 | <a href="https://github.com/oschettler/knowfox/issues">Open issues</a>
