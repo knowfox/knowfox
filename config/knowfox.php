c2e6c3a8c33f5f541ff9fc3433e376ee38ee242e
Matthias Niess <mniess@gmail.com>
2018-02-08 20:22:46 +0100

    Mention MERCURY_KEY in .env.example

--- config/knowfox.php
+++ config/knowfox.php
@@ -10,6 +10,6 @@ return [
 
     'types' => [],
 
-    'mercury_key' => env('MERCURY_KEY', 'secret key for https://mercury.postlight.com/web-parser/'),
+    'mercury_key' => env('MERCURY_KEY', 'secret key'),
     'presentation_base_path' => env('PRESENTATION_BASE', base_path()),
 ];
