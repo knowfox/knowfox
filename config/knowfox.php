08788b23d0c8da8a69219dc0d3d6aa56142ea692
Olav Schettler <olav@schettler.net>
2019-01-14 15:32:47 +0100

    Compiled Javascript

---
+++ config/knowfox.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'relationships' => [],
+
+    'languages' => [
+        'de',
+        'en',
+    ],
+
+    'types' => [
+        'concept',
+        'ebook',
+        'book list',
+        'journal',
+        'impact map',
+        'folder',
+        'feed item',
+        'website',
+    ],
+
+    'mercury_key' => env('MERCURY_KEY', 'secret key'),
+    'presentation_base_path' => env('PRESENTATION_BASE', base_path()),
+];
