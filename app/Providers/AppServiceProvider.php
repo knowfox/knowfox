69d8ec2a0715299e06a3689bfd0978d0e31059c1
Olav Schettler <olav@schettler.net>
2019-01-18 14:16:17 +0100

    Installed Telescope; fixed missing image styles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Telescope\TelescopeServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
     }
 }
