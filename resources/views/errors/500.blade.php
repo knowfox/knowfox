4a09c4ec3a94976a31bbdda7f50597d01c7cf8d6
Olav Schettler <olav@schettler.net>
2019-03-04 23:37:06 +0100

    Mask error on func_get_args()

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -14,7 +14,7 @@
 
         <p>Sorry for this.</p>
 
-        <?php error_log(json_encode(func_get_args()), 3, '/tmp/knowfox-500.log'); ?>
+        <?php error_log(json_encode(@func_get_args()), 3, '/tmp/knowfox-500.log'); ?>
 
     </main>
 @endsection
