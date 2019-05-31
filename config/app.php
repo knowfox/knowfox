ad174dfda694b9a88472db9e11ac88a54347bfc8
Olav Schettler <olav@schettler.net>
2019-05-31 11:10:41 +0200

    Added Core, Passwordless as packages with dependencies

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Knowfox\Passwordless\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -174,6 +175,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        Knowfox\Core\ServiceProvider::class,
 
     ],
 
