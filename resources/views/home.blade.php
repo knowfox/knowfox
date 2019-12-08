54da9785ea1b3b98b16b8159c1a1fac0ff4dc365
Olav Schettler <olav@schettler.net>
2019-12-08 20:55:27 +0100

    Buy me a coffee

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,9 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             @include('partials.search-form')
+	</div>
+	<div class="col-md-8 col-md-offset-2 text-center" style="margin-top:20px">
+            <a target="_blank" href="https://www.buymeacoffee.com/knowfox"><img src="/img/logo-bmc.svg" width="200"></a>
         </div>
     </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Tinkerthon</title>
+        <title>Knowfox</title>
 
         <meta name="description" content="Personal knowledge management">
         <meta name="google-site-verification" content="{{env('GOOGLE_SITE')}}" />
