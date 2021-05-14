9542f69f51a0cc722eaa4d5a36afbeac0aeb5be3
Olav Schettler <olav@schettler.net>
2021-05-14 13:02:40 +0200

    Removed Product Hunt teaser

--- resources/views/website/schettler_net/layout.blade.php
+++ resources/views/website/schettler_net/layout.blade.php
@@ -57,20 +57,3 @@
 
     </script>
 @endpush
-
-@push('scripts')
-    <script>
-        window.productHuntUpcoming = {
-            appId: 1433,
-            position: 'bottomLeft',
-        };
-
-        (function(doc, scr, src, a, b) {
-            a = doc.createElement(scr);
-            b = doc.getElementsByTagName(scr)[0];
-            a.async = true;
-            a.src = src;
-            b.parentNode.insertBefore(a, b);
-        })(document, 'script', 'https://assets.producthunt.com/assets/upwigloader.js');
-    </script>
-@endpush
